5dbaf7f0619a97667b912c7180042de4a7129dfb
root <root@localhost.localdomain>
2020-01-19 11:13:30 -0500

    actualizados ficheros de format string

--- formatString/fmt.c
+++ formatString/fmt.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 void main (int argc, char *argv[]) {
 
 	char buffer[1024];
 	char cadena[8] = "prueba";
 
+	printf ("Direccion de cadena: %p\n",cadena);
+	read (0, buffer, 1024);
+	
 	// Lo correcto seria escribir: 
 	// printf ("Has escrito: %s\n", argv[1]);
 	// Pero en cambio escribimos: 
 
-	strncpy (buffer, argv[1],1023);
-	printf ("Direccion de cadena: %p\n",cadena);
 	printf ("Has escrito: "); 
 	printf (buffer);
 	printf ("\n");
