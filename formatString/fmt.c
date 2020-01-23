b0d07bffc35797725e699b4771a0b8a75166c3d9
Gabriel Maciá <gmacia@ugr.es>
2020-01-23 12:03:28 -0500

    modificado ejemplo format.string

--- formatString/fmt.c
+++ formatString/fmt.c
@@ -7,13 +7,16 @@
 #include <string.h>
 #include <unistd.h>
 
-
 void main (int argc, char *argv[]) {
 
 	char buffer[1024];
 	char cadena[8] = "prueba";
 
+	setvbuf(stdin, NULL, _IONBF, 0);
+	setvbuf(stdout, NULL, _IONBF, 0);	
+
 	printf ("Direccion de cadena: %p\n",cadena);
+	printf ("Escribe algo> ");
 	read (0, buffer, 1024);
 	
 	// Lo correcto seria escribir: 
@@ -24,5 +27,4 @@ void main (int argc, char *argv[]) {
 	printf (buffer);
 	printf ("\n");
 	printf ("Cadena: %s\n", cadena);
-
 }
