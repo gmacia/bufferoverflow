ac0def74a31a33635275598ad2c5412aa20a4bd4
Gabriel Maciá-Fernańdez <gmacia@ugr.es>
2022-02-20 15:23:35 -0500

    Modified fmt.c to fix error writing without \0 in last byte of buf

--- formatString/retoBasico/fmt.c
+++ formatString/retoBasico/fmt.c
@@ -11,14 +11,16 @@ void main (int argc, char *argv[]) {
 
 	char buffer[1024];
 	char cadena[8] = "prueba";
-
+	int n; 
+	
 	setvbuf(stdin, NULL, _IONBF, 0);
 	setvbuf(stdout, NULL, _IONBF, 0);	
 
 	printf ("Direccion de cadena: %p\n",cadena);
 	printf ("Escribe algo> ");
-	read (0, buffer, 1024);
-	
+	n = read (0, buffer, 1024);
+	buffer [n] = '\0';
+		
 	// Lo correcto seria escribir: 
 	// printf ("Has escrito: %s\n", argv[1]);
 	// Pero en cambio escribimos: 
