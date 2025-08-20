b60ab82bf625ad75be2e756bcf7c7c540cce614c
Gabriel Maciá <gmacia@ugr.es>
2025-08-20 05:02:08 -0400

    Modified installation scripts to correct errors

--- installation/signaturesKali.sh
+++ installation/signaturesKali.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0  ]
+	then echo -e "\e[31m[+]\e[0m \e[32mPlease run as root\e[0m"
+	exit
+fi
+
 wget -q -O - archive.kali.org/archive-key.asc | apt-key add
+
+echo -e "[+] \e[32mSignatures updated\e[0m"
